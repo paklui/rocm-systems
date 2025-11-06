fdf8f91d36892b917b8430db7dca4cf5ea78ed0e
Giovanni <gbaraldi@amd.com>
2025-11-06 13:48:29 -0600

    Typo fix

--- projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/thread_trace/service.cpp
+++ projects/rocprofiler-sdk/source/lib/rocprofiler-sdk/thread_trace/service.cpp
@@ -92,7 +92,8 @@ build_pack_from_array(parameter_pack&                             pack,
             case ROCPROFILER_THREAD_TRACE_PARAMETER_BUFFERING_MODE:
                 if(param.value >= ROCPROFILER_THREAD_TRACE_PARAMETER_BUFFERING_MODE_LAST)
                     return ROCPROFILER_STATUS_ERROR_INVALID_ARGUMENT;
-                pack.triple_buffering = param.value != 0;
+                pack.triple_buffering =
+                    param.value == ROCPROFILER_THREAD_TRACE_PARAMETER_BUFFERING_MODE_TRIPLE_BUFFER;
                 break;
             case ROCPROFILER_THREAD_TRACE_PARAMETER_LAST:
                 return ROCPROFILER_STATUS_ERROR_INVALID_ARGUMENT;
